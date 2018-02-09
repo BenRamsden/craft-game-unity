9949f58ea93dff02c3eaf968632f0cf437e80cb8
Antony Lloyd <antony@Antonys-MacBook-Pro.local>
2018-02-09 12:01:04 +0000

    Spawned some cubes in the startup script

--- craftgame/Assets/Scripts/Startup.cs
+++ craftgame/Assets/Scripts/Startup.cs
@@ -9,7 +9,26 @@ public class Startup
 	// Use this for initialization
 	static Startup()
 	{
-		Debug.Log ("Why hello there!");
+		for (int x = 0; x < 8; x++)
+		{
+			for (int y = 0; y < 8; y++)
+			{
+				for (int z = 0; z < 8; z++)
+				{
+					Color c;
+					if (y <= 3)
+						c = new Color (0, 0, 0);
+					else if (y <= 6)
+						c = new Color (0.65f, 0.15f, 0.15f);
+					else
+						c = Color.green;
+
+					GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+					cube.transform.position = new Vector3 (x + 10, y + 10, z + 10);
+					cube.GetComponent<Renderer> ().material.color = c;
+				}
+			}
+		}
 	}
 
 }
