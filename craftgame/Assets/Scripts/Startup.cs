ae22ff516ef72dbea59039100771c3a68af74d52
Antony Lloyd <antony@Antonys-MacBook-Pro.local>
2018-02-09 11:36:15 +0000

    Created startup script

---
+++ craftgame/Assets/Scripts/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class Startup
+{
+	// Use this for initialization
+	static Startup()
+	{
+		Debug.Log ("Why hello there!");
+	}
+
+}
