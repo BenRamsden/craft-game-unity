61d8aec963d082509ebda59921ba09713e031a85
BenRamsden <benramsdensii@gmail.com>
2018-02-13 15:29:21 +0000

    Added impulse double jump on press space

--- ChunkTest/Assets/PlayerMove.cs
+++ ChunkTest/Assets/PlayerMove.cs
@@ -6,16 +6,22 @@ public class PlayerMove : MonoBehaviour {
 
 	public Rigidbody rb;
 
+	public Vector3 jump;
+	public bool isGrounded;
+
+	public float moveSpeed = 10.0F;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		Vector3 rot = transform.localRotation.eulerAngles;
 		rotY = rot.y;
 		rotX = rot.x;
+
+		jump = new Vector3(0.0f, 5.0f, 0.0f);
 	}
 
 	/** Moving player using unity physics */
-	public float moveSpeed = 10.0F;
 	void FixedUpdate () {
 		float moveX = Input.GetAxis ("Horizontal");
 		float moveZ = Input.GetAxis ("Vertical");
@@ -25,6 +31,11 @@ public class PlayerMove : MonoBehaviour {
 		rb.transform.Translate(movement * moveSpeed * Time.deltaTime);
 	}
 
+
+	void OnCollisionStay()
+	{
+		isGrounded = true;
+	}
 
 	/** Rotating player with mouse*/
 	private float mouseX, mouseY;
@@ -40,6 +51,12 @@ public class PlayerMove : MonoBehaviour {
 		//stop the player being able to look up/down too much
 		//rotX = Mathf.Clamp(rotX, -x_AxisRotateClamp, x_AxisRotateClamp);
 		rb.transform.rotation= Quaternion.Euler(0.0f, rotY, 0.0f);
+
+		if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+
+			rb.AddForce(jump, ForceMode.Impulse);
+			isGrounded = false;
+		}
 	}
 
 
