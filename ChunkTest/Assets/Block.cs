16babe3555bd81cccceb0077d874372ab20a3d53
ConnorT <c.a.ts@live.co.uk>
2018-02-14 19:58:29 +0000

    Added test code,  Added comments
    Generally neatened things up, added a small piece of test code to the bottom of the procedural generation step in Chunk.cs
    Also we now just check the highest point in the chunk when drawing in the y-axis, instead of maintaining a bool array of empty layer markers.

--- ChunkTest/Assets/Block.cs
+++ ChunkTest/Assets/Block.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block{
+public class Block
+{
     GameObject thisBody;
 	bool isMineable, isPickupable, isBreakable, isTraversable;
 	int blockHealth, blockDamage;
@@ -10,33 +11,60 @@ public class Block{
     int chunkX, chunkY, chunkZ;
     string blockType;
 
+    /**draw() 
+     * Draws the physical representation of a Block in the world.
+     */
     public void draw()
     {
         thisBody = GameObject.Instantiate(Resources.Load(blockType), new Vector3(worldX, worldY, worldZ), Quaternion.identity) as GameObject;
     }
 
-	public void setPosition(int row, int layer, int column)
+    /**setPosition(int,int,int)
+     * Sets the Block's knowledge of its position in the world.
+     * @param row - the x coordinate of where the Block is in the world.
+     * @param layer - the y coordinate of where the Block is in the world.
+     * @param column - the z coordinate of where the Block isin the world.
+     */
+    public void setPosition(int row, int layer, int column)
     {
         worldX = row;
         worldY = layer;
         worldZ = column;
 	}
 
-    public void setChunkPosition(int posX, int posY, int posZ){
+    /**setChunkPosition(int,int,int)
+     * Sets the Block's knowledge of its position in its Chunk.
+     * @param row - the x coordinate of where the Block is in its Chunk.
+     * @param layer - the y coordinate of where the Block is in its Chunk.
+     * @param column - the z coordinate of where the Block is in its Chunk.
+     */
+    public void setChunkPosition(int posX, int posY, int posZ)
+    {
         chunkX = posX;
         chunkY = posY;
         chunkZ = posZ;
     }
 
+    /**getChunkX()
+     * @return chunkX - the x coordinate of where the Block is in its Chunk.
+     */
     public int getChunkX()
     {
         return chunkX;
     }
+
+    /**getChunkY()
+     * @return chunkZ - the z coordinate of where the Block is in its Chunk.
+     */
     public int getChunkZ()
     {
         return chunkZ;
     }
 
+    /**setBlockType(string)
+     * Sets the type of Block.
+     * @param blockType - the type of prefab this Block should draw.
+     */
     public void setBlockType(string blockType)
     {
         this.blockType = blockType;
--- ChunkTest/Assets/Chunk.cs
+++ ChunkTest/Assets/Chunk.cs
@@ -2,30 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chunk{
+public class Chunk
+{
 	private const int CHUNK_SIZE = 16;
-	//blocks is a 3D array in the form [x, y, z], as such each layer is
-	//up the y axis like floors of a building. and layerIsAir therefore
-	//is a direct reference such that: blocks[-,x,-] = layersIsAir[x]
+
+	//blocks[,,] is a 3D array in the form [x, y, z]
 	private Block[,,] blocks = new Block[CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE];
-	private bool[] layerIsAir = new bool[CHUNK_SIZE];
+    private int highestPoint = 0;
 	private int offsetX, offsetZ;
 
 	public Chunk(int offsetX, int offsetZ){
 		this.offsetX = offsetX;
 		this.offsetZ = offsetZ;
 
-        /*START SECTION*/
-        /*This section intialises the chunk, in future this will
-		 *be done by the procedural generation algorithm.*/
+        /* --- START SECTION --- */
+        /*This section intialises the chunk*/
 
         Block tempBlock;
         int world_x, world_y, world_z;
         float perlinX, perlinY, perlinZ;
 
-		for (int x = 0; x < CHUNK_SIZE; x++) {
-			for (int y = 0; y < CHUNK_SIZE; y++) {
-				for (int z = 0; z < CHUNK_SIZE; z++) {
+		for (int x = 0; x < CHUNK_SIZE; x++)
+        {
+			for (int y = 0; y < CHUNK_SIZE; y++)
+            {
+				for (int z = 0; z < CHUNK_SIZE; z++)
+                {
                     //Calculate absolute position of block (world space)
                     world_x = offsetX + x;
                     world_z = offsetZ + z;
@@ -39,45 +41,63 @@ public class Chunk{
                     world_y = y + (int)(perlinY * 5);
 
 					//Debug.Log ("world_x:" + world_x + " world_z:" + world_z + " = " + world_y);
-
-					if (world_y < 0 || world_y > CHUNK_SIZE-1) {
+					if (world_y < 0 || world_y > CHUNK_SIZE-1)
+                    {
 						Debug.Log ("Cannot insert chunk into block at index " + world_y + " continuing");
 						continue;
 					}
 
-					if (y <= 0) {
+					if (y <= 0)
+                    {
 						tempBlock = new Block();
 						tempBlock.setBlockType("StoneBlock");
-					} else if (y <= 1) {
+					}
+                    else if (y <= 1)
+                    {
 						tempBlock = new Block();
 						tempBlock.setBlockType("GrassBlock");
-					} else {
+					}
+                    else
+                    {
 						continue;
 					}
+
+                    highestPoint = (world_y > highestPoint) ? world_y : highestPoint;
 
 					tempBlock.setPosition(world_x, world_y, world_z);
                     tempBlock.setChunkPosition(offsetX, 0, offsetZ);
 					blocks [x, world_y, z] = tempBlock;
-				}
+
+                    //Test to fill in all layers below grass with stone (can be deleted when necessary)
+                    if (y == 0)
+                    {
+                        for (int i = 0; i < world_y; i++)
+                        {
+                            tempBlock = new Block();
+                            tempBlock.setBlockType("StoneBlock");
+                            tempBlock.setPosition(world_x, i, world_z);
+                            tempBlock.setChunkPosition(offsetX, 0, offsetZ);
+                            blocks[x, i, z] = tempBlock;
+                        }
+                    }
+                }
 			}
 		}
-	/*END SECTION*/
+	    /* --- END SECTION --- */
 		drawChunk();
 	}
 
-	//Renders the visible blocks based on the current state of the chunk.
-	//Currently this means rendering any block that touches a null "air"
-	//block.
+    /**drawChunk()
+     * Renders the visible blocks based on the current state of the chunk.
+     * Currently this means rendering any block that touches a null "air" Block.
+     */
 	private void drawChunk()
 	{
 		Block blockToDraw;
-		//Iterate through each layer (y-axis)
-		for (int y = CHUNK_SIZE-1; y >= 0; y--)
+
+		//Iterate through each layer from the highest point downwards (y-axis)
+		for (int y = highestPoint; y >= 0; y--)
 		{
-			//If the current layer is completely air, continue to the next
-			//layer down, there is no point iterating through that layer as
-			//there is nothing to draw.
-			//if(layerIsAir[l]) continue;
 			//Iterate through each row (x-axis)
 			for (int x = 0; x < CHUNK_SIZE; x++)
 			{
@@ -87,6 +107,7 @@ public class Chunk{
 					if (blocks [x, y, z] != null)
 					{
 						blockToDraw = blocks [x, y, z];
+
                         //If statement to handle Blocks at the edge of a chunk
                         if (x == 0 || x == CHUNK_SIZE-1 ||
                             y == 0 || y == CHUNK_SIZE-1 ||
@@ -96,8 +117,8 @@ public class Chunk{
                         }
                         //If statement to handle Blocks within a chunk
                         else if(blocks[x + 1 , y, z] == null || blocks[x - 1, y, z] == null ||
-                                blocks[x, y + 1, z] == null || blocks[x, y - 1, z] == null ||
-                                blocks[x, y, z + 1] == null || blocks[x, y, z - 1] == null)
+                                blocks[x, y + 1, z] == null  || blocks[x, y - 1, z] == null ||
+                                blocks[x, y, z + 1] == null  || blocks[x, y, z - 1] == null)
                         {
                             blockToDraw.draw();
                         }
@@ -107,16 +128,13 @@ public class Chunk{
 		}
 	}
 
-
 	// Use this for initialization
 	void Start()
-	{
-		
+	{	
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
 	}
 }
