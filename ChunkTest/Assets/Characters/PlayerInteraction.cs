ad3ccfdda109931bcf7ca9beb414769d8d97fe4b
ConnorT <c.a.ts@live.co.uk>
2018-02-24 00:01:36 +0000

    Fixed irregular block damage bug

--- ChunkTest/Assets/Characters/PlayerInteraction.cs
+++ ChunkTest/Assets/Characters/PlayerInteraction.cs
@@ -3,50 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour {
-	Rigidbody rb;
-	GameObject world;
 	RaycastHit hit;
 	bool isLeftMouseDown;
 	Block currentBlock;
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody>();
-		//world = GameObject.Find("World");
-
-		//chunk = world.findChunk (currentPosition);
-		//Block = Chunk.findblock (currentposition);
-
-	}
-
 	void FixedUpdate() {
+        Vector3 worldPosition, posOfChunk, posOfBlock;
 		if (isLeftMouseDown) {
 			if (Physics.Raycast (transform.position, transform.forward, out hit, 5)) {
-				if (hit.collider.gameObject.CompareTag ("GrassBlock")) {
-					Vector3 worldPosition = hit.collider.gameObject.GetComponent<Transform> ().position;
-					Vector3 moduloVector = new Vector3 (worldPosition.x % 16.0f, worldPosition.y % 16.0f, worldPosition.z % 16.0f);
-					Vector3 positionOfChunk = worldPosition - (moduloVector);
-					Vector3 positionOfBlockInChunk = moduloVector;
+                if (hit.collider.gameObject.CompareTag("GrassBlock")) {
+                    worldPosition = hit.collider.gameObject.GetComponent<Transform>().position;
+                    posOfBlock = new Vector3(worldPosition.x % 16.0f, worldPosition.y % 16.0f, worldPosition.z % 16.0f);
+                    posOfChunk = worldPosition - (posOfBlock);
 
-					Chunk currentChunk = GameObject.Find ("World").GetComponent<WorldGenerator> ().getPGenerator ().getChunk (positionOfChunk);
-
-					currentBlock = currentChunk.getBlock (positionOfBlockInChunk);
-
-					Debug.Log ("There is something in front of the object!");
-				}
-			} else {
-				currentBlock = null;
+                    Chunk currentChunk = GameObject.Find("World").GetComponent<WorldGenerator>().getPGenerator().getChunk(posOfChunk);
+                    currentBlock = currentChunk.getBlock(posOfBlock);
+                }
 			}
-		}
+		}else {
+            currentBlock = null;
+        }
 
-	}
+    }
 
 	// Update is called once per frame
 	void Update () {
 		isLeftMouseDown = Input.GetMouseButtonDown(0);
-		if (isLeftMouseDown && currentBlock != null) {
-			currentBlock.damageBlock (10);
-		}
-
+        if (currentBlock != null) {
+            currentBlock.damageBlock(10);
+        }
 	}
 }
