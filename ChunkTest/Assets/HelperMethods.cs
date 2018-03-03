818f450f3eb75f30eb1d067475e09289109b7b6d
BenRamsden <benramsdensii@gmail.com>
2018-03-03 15:51:42 +0000

    Created a helper method to get chunk position from world position

---
+++ ChunkTest/Assets/HelperMethods.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelperMethods
+{
+	public HelperMethods ()
+	{
+	}
+
+	public static Vector3 worldPositionToChunkPosition(Vector3 worldPosition) {
+		float originX = worldPosition.x;
+		originX -= originX % Chunk.CHUNK_SIZE;
+
+		float originY = worldPosition.y;
+		originY -= originY % Chunk.CHUNK_SIZE;
+
+		float originZ = worldPosition.z;
+		originZ -= originZ % Chunk.CHUNK_SIZE;
+
+		return new Vector3 (originX, originY, originZ);
+	}
+}
+
