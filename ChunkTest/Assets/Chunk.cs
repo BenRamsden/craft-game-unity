4c0a4551796d884a9ef3b8d8b1b432d0bbe36a4b
BenRamsden <benramsdensii@gmail.com>
2018-03-23 15:16:36 +0000

    Added desert regions

--- ChunkTest/Assets/Chunk.cs
+++ ChunkTest/Assets/Chunk.cs
@@ -52,6 +52,8 @@ public class Chunk
 					float perlinX = ((float)worldX) / CHUNK_SIZE;
 					float perlinZ = ((float)worldZ) / CHUNK_SIZE;
 
+					float perlinTerrainType = Mathf.PerlinNoise (perlinX / 100, perlinZ / 100);
+
 					float perlinTerrain = Mathf.PerlinNoise (perlinX / 10, perlinZ / 10) * 2; //perlin between 0.0 and 1.0
 					perlinTerrain = Mathf.Pow (perlinTerrain, 3);
 
@@ -66,12 +68,12 @@ public class Chunk
 
                     string blockType = null;
 
-					if (worldY <= 12){
-                        blockType = "StoneBlock";
-					} else {
+					if (perlinTerrainType < 0.3f) {
 						blockType = "FastGrass";
+					} else {
+						blockType = "FastDirt";
 					}
-
+						
                     highestPoint = (perlinY > highestPoint) ? perlinY : highestPoint;
 
                     CreateBlock(blockType, x, perlinY, z);
