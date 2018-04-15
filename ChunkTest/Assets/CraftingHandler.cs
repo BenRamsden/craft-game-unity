3993fc76d511d49785be62bf2658ae6cc289c0ed
ConnorT <c.a.ts@live.co.uk>
2018-04-15 16:04:11 +0100

    Added a sample receipes JSON file

--- ChunkTest/Assets/CraftingHandler.cs
+++ ChunkTest/Assets/CraftingHandler.cs
@@ -6,12 +6,8 @@ public static class CraftingHandler{
 
 	public static Item craftItem(Container matrix){
 		Block block = new Block();
-		Debug.Log ("1");
 		if (matrix.getItem (0) != null){
-			Debug.Log ("2");
-			Debug.Log (matrix.getItem (0).resourceString);
 			if(matrix.getItem(0).resourceString == "LogBlock"){
-				Debug.Log ("3");
 				block.resourceString = "WoodBlock";
 				return block;
 			}
